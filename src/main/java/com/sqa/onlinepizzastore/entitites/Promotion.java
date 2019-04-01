d52d1a22a09c5023882ac536fdeaaedc1b73aba8
ciprian19 <bciprian.constantin@yahoo.com>
2019-04-01 23:39:20 +0300

    update

---
+++ src/main/java/com/sqa/onlinepizzastore/entitites/Promotion.java
@@ -0,0 +1,17 @@
+package com.sqa.onlinepizzastore.entitites;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+
+public class Promotion {
+
+	private Long promId;
+	private String promName;
+	private Double value;
+	private Double percentage;
+	
+	
+	
+}
