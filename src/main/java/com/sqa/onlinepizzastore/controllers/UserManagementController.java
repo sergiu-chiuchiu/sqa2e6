58e6660102b83c82997fcb6236d765534ab04f6b
Sergiu Chiuchiu <sergiu.chiuchiu@gmail.com>
2019-04-08 09:52:08 +0300

    fixed alert on index page

--- src/main/java/com/sqa/onlinepizzastore/controllers/UserManagementController.java
+++ src/main/java/com/sqa/onlinepizzastore/controllers/UserManagementController.java
@@ -15,6 +15,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -119,6 +120,14 @@ public class UserManagementController {
 	public String viewUsers(Model model) {
 		model.addAttribute("AppUsers", appUserService.getAllUsers());
 		return "ViewUsers";
+	}
+	
+	@GetMapping(value = "/users/delete/{email}")
+	public String deleteUser(@PathVariable("email") String email) {
+		AppUser appUserToDelete = appUserService.getAppUserByEmail(email);
+		appUserService.deleteAppUser(appUserToDelete);
+		
+		return "redirect:/user/users";
 	}
 
 	@GetMapping(value = "/adduser")
